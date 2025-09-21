9c5a56e9f421499b1476d0d657dc88869bee9fbb
Jan Bouwman <49314613+uwbanjoman@users.noreply.github.com>
2025-09-21 13:54:13 +0200

    added include serialization to PowerGridModel.jl

--- src/PowerGridModel.jl
+++ src/PowerGridModel.jl
@@ -9,6 +9,7 @@ include("metadata.jl")
 include("dataset.jl")
 include("model.jl")
 include("options.jl")
+include("serialization.jl")
 
 function lib_pgm_location()
     power_grid_model_c_jll.libpower_grid_model_c

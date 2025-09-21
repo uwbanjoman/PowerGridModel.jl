5a751100b5348c398e343f5c91969323b6c337a5
Jan Bouwman <49314613+uwbanjoman@users.noreply.github.com>
2025-09-21 13:49:49 +0200

    added include options to PowerGridModel.jl

--- src/PowerGridModel.jl
+++ src/PowerGridModel.jl
@@ -8,6 +8,7 @@ include("buffer.jl")
 include("metadata.jl")
 include("dataset.jl")
 include("model.jl")
+include("options.jl")
 
 function lib_pgm_location()
     power_grid_model_c_jll.libpower_grid_model_c

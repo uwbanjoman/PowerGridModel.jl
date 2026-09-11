2b32475ccccea76a7a4d05611d698b275432eb69
Jan Bouwman <49314613+uwbanjoman@users.noreply.github.com>
2026-09-11 21:09:13 +0200

    Enhance serialization functions with documentation
    Updated the serialization functions to include detailed documentation and improved function signatures.

--- src/serialization.jl
+++ src/serialization.jl
@@ -1,38 +1,141 @@
+# ssrc/serialization.jl
+"""
+    PGM_create_deserializer_from_binary_buffer(handle, data, size, serialization_format)
 
-function PGM_create_deserializer_from_binary_buffer(handle::Ptr{Cvoid}, data_ptr::Ptr{UInt8}, size::PGM_Idx, serialization_format::PGM_Idx)
-        ccall((:PGM_create_deserializer_from_binary_buffer, pgm_lib), Ptr{PGM_Deserializer}, (Ptr{Cvoid}, Ptr{UInt8}, PGM_Idx, PGM_Idx), handle, data_ptr, size, serialization_format)
-    return deserializer
-    end
-    
-    function PGM_create_deserializer_from_null_terminated_string(handle::Ptr{Cvoid}, data_string::Cstring, serialization_format::PGM_Idx)
-        ccall((:PGM_create_deserializer_from_null_terminated_string, pgm_lib), Ptr{PGM_Deserializer}, (Ptr{Cvoid}, Cstring, PGM_Idx), handle, data_string, serialization_format)
-    end
+Create a deserializer from binary buffer/byte stream.
 
-    function PGM_deserializer_get_dataset(handle::Ptr{Cvoid}, deserializer::Ptr{PGM_Deserializer})
-        ccall((:PGM_deserializer_get_dataset, pgm_lib), Ptr{PGM_WritableDataset}, (Ptr{Cvoid}, Ptr{PGM_Deserializer}), handle, deserializer)
-    end
+# Arguments
+* `handle`:
+* `data`: The pointer to the byte stream.
+* `size`: The size of the byte stream.
+* `serialization_format`: The desired data format of the serialization. See #[`PGM_SerializationFormat`](@ref) .
+# Returns
+A pointer to the deserializer instance. Should be freed by [`PGM_destroy_deserializer`](@ref)(). Returns NULL if errors occured (check the handle for error information).
+"""
+function PGM_create_deserializer_from_binary_buffer(handle, data, size, serialization_format)
+    ccall((:PGM_create_deserializer_from_binary_buffer, libpower_grid_model_c), Ptr{PGM_Deserializer}, (Ptr{PGM_Handle}, Ptr{Cchar}, PGM_Idx, PGM_Idx), handle, data, size, serialization_format)
+end
 
-    function PGM_deserializer_parse_to_buffer(handle::Ptr{Cvoid}, deserializer::Ptr{PGM_Deserializer})
-        ccall((:PGM_deserializer_parse_to_buffer, pgm_lib), Cvoid, (Ptr{Cvoid}, Ptr{PGM_Deserializer}), handle, deserializer)
-    end
+"""
+    PGM_create_deserializer_from_null_terminated_string(handle, data_string, serialization_format)
 
-    function PGM_destroy_deserializer(deserializer::Ptr{PGM_Deserializer})
-    ccall((:PGM_destroy_deserializer, pgm_lib), Cvoid, (Ptr{PGM_Deserializer},), deserializer)
-    end
+Create a deserializer from a null terminated C string.
 
-    function PGM_create_serializer(handle::Ptr{Cvoid}, dataset::Ptr{PGM_ConstDataset}, serialization_format::PGM_Idx)
-        ccall((:PGM_create_serializer, pgm_lib), Ptr{PGM_Serializer}, (Ptr{Cvoid}, Ptr{PGM_ConstDataset}, PGM_Idx), handle, dataset, serialization_format)
-    end
-    # PGM.PGM_create_serializer(handle,dset_ptr,0)
+# Arguments
+* `handle`:
+* `data_string`: The pointer to the null-terminated C string.
+* `serialization_format`: The desired data format of the serialization. See #[`PGM_SerializationFormat`](@ref) .
+# Returns
+A pointer to the deserializer instance. Should be freed by [`PGM_destroy_deserializer`](@ref)(). Returns NULL if errors occured (check the handle for error information).
+"""
+function PGM_create_deserializer_from_null_terminated_string(handle, data_string, serialization_format)
+    ccall((:PGM_create_deserializer_from_null_terminated_string, libpower_grid_model_c), Ptr{PGM_Deserializer}, (Ptr{PGM_Handle}, Ptr{Cchar}, PGM_Idx), handle, data_string, serialization_format)
+end
 
-    function PGM_serializer_get_to_binary_buffer(handle::Ptr{Cvoid}, serializer::Ptr{PGM_Serializer}, use_compact_list::PGM_Idx, data::Ptr{Cvoid}, size::PGM_Idx)
-        ccall((:PGM_serializer_get_to_binary_buffer, pgm_lib), Cvoid, (Ptr{Cvoid}, Ptr{PGM_Serializer}, PGM_Idx, Ptr{Ptr{Cchar}}, PGM_Idx), handle, serializer, use_compact_list, data, size)
-    end
+"""
+    PGM_deserializer_get_dataset(handle, deserializer)
 
-    function PGM_serializer_get_to_zero_terminated_string(handle::Ptr{Cvoid}, serializer::Ptr{PGM_Serializer}, use_compact_list::PGM_Idx, indent::PGM_Idx)
-        ccall((:PGM_serializer_get_to_zero_terminated_string, pgm_lib), Ptr{Cchar}, (Ptr{Cvoid}, Ptr{PGM_Serializer}, PGM_Idx, PGM_Idx), handle, serializer, use_compact_list, indent)
-    end
+Get the [`PGM_WritableDataset`](@ref) object from the deserializer.
 
-    function PGM_destroy_serializer(serializer::Ptr{PGM_Serializer})
-        ccall((:PGM_destroy_serializer, pgm_lib), Cvoid, (Ptr{PGM_Serializer},), serializer)
-    end
+# Arguments
+* `handle`:
+* `deserializer`: The pointer to the deserializer.
+# Returns
+A pointer the instance of [`PGM_WritableDataset`](@ref). The pointer has the same lifetime as the deserializer. Use PGM\\_writable\\_dataset\\_get\\_info() to get the information of the dataset. Use PGM\\_writable\\_dataset\\_set\\_buffer() to set buffer.
+"""
+function PGM_deserializer_get_dataset(handle, deserializer)
+    ccall((:PGM_deserializer_get_dataset, libpower_grid_model_c), Ptr{PGM_WritableDataset}, (Ptr{PGM_Handle}, Ptr{PGM_Deserializer}), handle, deserializer)
+end
+
+"""
+    PGM_deserializer_parse_to_buffer(handle, deserializer)
+
+Parse the dataset and write to the user-provided buffers. The buffers must be set through PGM\\_writable\\_dataset\\_set\\_buffer().
+
+# Arguments
+* `handle`:
+* `deserializer`: The pointer to the deserializer
+# Returns
+No return value; check handle for error.
+"""
+function PGM_deserializer_parse_to_buffer(handle, deserializer)
+    ccall((:PGM_deserializer_parse_to_buffer, libpower_grid_model_c), Cvoid, (Ptr{PGM_Handle}, Ptr{PGM_Deserializer}), handle, deserializer)
+end
+
+"""
+    PGM_destroy_deserializer(deserializer)
+
+Destory deserializer
+
+# Arguments
+* `deserializer`: pointer to deserializer
+# Returns
+"""
+function PGM_destroy_deserializer(deserializer)
+    ccall((:PGM_destroy_deserializer, libpower_grid_model_c), Cvoid, (Ptr{PGM_Deserializer},), deserializer)
+end
+
+"""
+    PGM_create_serializer(handle, dataset, serialization_format)
+
+Create a serializer object based on input dataset, the buffers must be set in advance.
+
+# Arguments
+* `handle`:
+* `dataset`: A pointer to an instance of [`PGM_ConstDataset`](@ref)
+* `serialization_format`: The desired data format of the serialization. See #[`PGM_SerializationFormat`](@ref) .
+# Returns
+A pointer to the new serializer object. Should be freed by [`PGM_destroy_serializer`](@ref)() Returns NULL if errors occured (check the handle for error information).
+"""
+function PGM_create_serializer(handle, dataset, serialization_format)
+    ccall((:PGM_create_serializer, libpower_grid_model_c), Ptr{PGM_Serializer}, (Ptr{PGM_Handle}, Ptr{PGM_ConstDataset}, PGM_Idx), handle, dataset, serialization_format)
+end
+# pgm.create_serializer(handle,dset_ptr,0)
+
+"""
+    PGM_serializer_get_to_binary_buffer(handle, serializer, use_compact_list, data, size)
+
+Serialize the dataset into a binary buffer format.
+
+# Arguments
+* `handle`:
+* `serializer`: A pointer to an existing serializer.
+* `use_compact_list`: 1 for use compact list per element of serialization; 0 for use dictionary per element.
+* `data`: Output argument: the data pointer of the packed buffer will be written to *data.
+* `size`: Output argument: the length of the packed buffer will be written to *size.
+# Returns
+No return value; check handle for error.
+"""
+function PGM_serializer_get_to_binary_buffer(handle, serializer, use_compact_list, data, size)
+    ccall((:PGM_serializer_get_to_binary_buffer, libpower_grid_model_c), Cvoid, (Ptr{PGM_Handle}, Ptr{PGM_Serializer}, PGM_Idx, Ptr{Ptr{Cchar}}, Ptr{PGM_Idx}), handle, serializer, use_compact_list, data, size)
+end
+
+"""
+    PGM_serializer_get_to_zero_terminated_string(handle, serializer, use_compact_list, indent)
+
+Serialize the dataset into a zero terminated C string. Only supported for uncompressed data formats.
+
+# Arguments
+* `handle`:
+* `serializer`: A pointer to an existing serializer.
+* `use_compact_list`: 1 for use compact list per element of serialization; 0 for use dictionary per element.
+* `indent`: The indentation of the JSON, use -1 for no indent and no new line (compact format).
+# Returns
+A NULL-terminated json string. Returns NULL if errors occured (check the handle for error information).
+"""
+function PGM_serializer_get_to_zero_terminated_string(handle, serializer, use_compact_list, indent)
+    ccall((:PGM_serializer_get_to_zero_terminated_string, libpower_grid_model_c), Ptr{Cchar}, (Ptr{PGM_Handle}, Ptr{PGM_Serializer}, PGM_Idx, PGM_Idx), handle, serializer, use_compact_list, indent)
+end
+
+"""
+    PGM_destroy_serializer(serializer)
+
+Destroy serializer.
+
+# Arguments
+* `serializer`: The pointer to the serializer.
+# Returns
+"""
+function PGM_destroy_serializer(serializer)
+    ccall((:PGM_destroy_serializer, libpower_grid_model_c), Cvoid, (Ptr{PGM_Serializer},), serializer)
+end

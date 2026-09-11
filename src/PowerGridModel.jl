0747d81a8fa8ff0b32471b0033f2776067702216
Jan Bouwman <49314613+uwbanjoman@users.noreply.github.com>
2026-09-11 21:30:44 +0200

    Disable lib_pgm_location function
    Comment out the lib_pgm_location function and its usage.

--- src/PowerGridModel.jl
+++ src/PowerGridModel.jl
@@ -7,11 +7,11 @@ using Printf
 const PGM_Idx = Int64
 const PGM_ID = Int32
 
-function lib_pgm_location()
-    power_grid_model_c_jll.libpower_grid_model_c
-end
+#function lib_pgm_location()
+#    power_grid_model_c_jll.libpower_grid_model_c
+#end
 
-pgm_lib = lib_pgm_location()
+#pgm_lib = lib_pgm_location()
 
 include("c_enums.jl")
 include("basics.jl")

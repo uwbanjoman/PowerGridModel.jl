292842a6265f24ec6a852e53423ce3543aeb5681
Jan Bouwman <49314613+uwbanjoman@users.noreply.github.com>
2026-09-16 20:45:20 +0200

    Clean up unused function and variable
    Removed commented-out function and library initialization.

--- src/PowerGridModel.jl
+++ src/PowerGridModel.jl
@@ -7,12 +7,6 @@ using Printf
 const PGM_Idx = Int64
 const PGM_ID = Int32
 
-#function lib_pgm_location()
-#    power_grid_model_c_jll.libpower_grid_model_c
-#end
-
-#pgm_lib = lib_pgm_location()
-
 include("c_enums.jl")
 include("basics.jl")
 include("handle.jl")
